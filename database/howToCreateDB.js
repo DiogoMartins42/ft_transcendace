444c1d00d5e3b9441596b8f3bf527d229b0a9483
Rodrigo1331 <rodrigocruzantunes@gmail.com>
2025-08-12 13:19:35 +0100

    hashing passwords more safely

--- database/howToCreateDB.js
+++ database/howToCreateDB.js
@@ -1,4 +1,4 @@
-const { addUser, initDB } = require('./init.js');
+const {addUser, initDB} = require('./init.js');
 
 // First, ensure DB and table exist
 initDB();
@@ -6,4 +6,13 @@ initDB();
 // Then insert your user (with a slight delay to allow table creation)
 setTimeout(() => {
   addUser("rcruz-an2222", "thisIsMySuperSecurePassword", "rcruz-an@gmail.com");
-}, 100); // 100ms is usually enough
+}, 100);
+
+//-----------------------------------------------------------------------------------------------------------------//
+
+// Checking if the password is correct
+const {verifyUserPassword} = require('./init.js');
+
+verifyUserPassword('rcruz-an2222', 'thisIsMySuperSecurePassword');
+
+//-----------------------------------------------------------------------------------------------------------------//
